b997ef893d28281cfa99b16a9a2dcbbc86d69640
ABSanthosh <a.b.santhosh02@gmail.com>
2024-09-19 00:55:39 -0400

    chore: Add global.d.ts for theme type and update dependencies

--- svelte.config.js
+++ svelte.config.js
@@ -1,17 +1,31 @@
 import adapter from '@sveltejs/adapter-auto';
-import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
+import { sveltePreprocess } from 'svelte-preprocess';
+import autoprefixer from 'autoprefixer';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
-	// Consult https://kit.svelte.dev/docs/integrations#preprocessors
-	// for more information about preprocessors
-	preprocess: vitePreprocess(),
+	preprocess: sveltePreprocess({
+		scss: {
+			prependData: `
+				@use "src/styles/root/_variables" as *;
+				@import "src/styles/root/_mixins.scss";
+			`
+		},
+		postcss: {
+			plugins: [autoprefixer()]
+		}
+	}),
 
 	kit: {
-		// adapter-auto only supports some environments, see https://kit.svelte.dev/docs/adapter-auto for a list.
-		// If your environment is not supported, or you settled on a specific environment, switch out the adapter.
-		// See https://kit.svelte.dev/docs/adapters for more information about adapters.
-		adapter: adapter()
+		adapter: adapter(),
+		alias: {
+			$components: 'src/lib/components',
+			$styles: 'src/styles/routes',
+			$utils: 'src/lib/utils',
+			$types: 'src/lib/types',
+			$stores: 'src/lib/stores/*',
+			$images: 'src/lib/images/*'
+		}
 	}
 };
 
