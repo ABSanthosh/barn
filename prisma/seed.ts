8b77e5b1215af3b24e9dbd96a58a4ad610416ace
ABSanthosh <a.b.santhosh02@gmail.com>
2024-09-22 09:57:36 -0400

    chore: Add onboarding pages and fix type generation issue

---
+++ prisma/seed.ts
@@ -0,0 +1,26 @@
+import { Prisma, PrismaClient } from '@prisma/client';
+
+import countries from '/home/santhosh/Desktop/Projects/TypeScript/2024/9_Barn/Data/countries.json';
+import recommended from '/home/santhosh/Desktop/Projects/TypeScript/2024/9_Barn/Data/recommended.json';
+
+const prisma = new PrismaClient();
+
+async function ImportCSV() {
+	return Promise.all([
+		prisma.topic.createMany({
+			data: recommended
+		}),
+
+		prisma.topic.createMany({
+			data: countries
+		})
+	]);
+}
+
+ImportCSV()
+	.catch((e) => {
+		console.error(e);
+	})
+	.finally(async () => {
+		await prisma.$disconnect();
+	});
