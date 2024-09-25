38a70f66e451c408218afb817da7ea5fec55c917
ABSanthosh <a.b.santhosh02@gmail.com>
2024-09-24 20:22:21 -0400

    chore: Update vite.config.ts to include test setup files and use jsdom environment

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,14 @@ import { defineConfig } from 'vitest/config';
 
 export default defineConfig({
 	plugins: [sveltekit()],
+	define: {
+		// Eliminate in-source test code
+		'import.meta.vitest': 'undefined'
+	},
 	test: {
-		include: ['src/**/*.{test,spec}.{js,ts}']
+		include: ['src/**/*.{test,spec}.{js,ts}'],
+		globals: true,
+		setupFiles: ['./setupTest.ts'],
+		environment: 'jsdom'
 	}
 });
